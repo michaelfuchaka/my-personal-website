32b526467fa68907575dd497d4080e6743366c26
michaelfuchaka <fuchakamichael06@gmail.com>
2025-06-02 11:21:34 +0300

    modifying my button code in index.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const container = document.getElementById("projectButtonContainer");
+
+  const button = document.createElement("button");
+  button.textContent = "Launch Project";
+
+  button.addEventListener("click", function () {
+    const viewLink = document.getElementById("view-link");
+    viewLink.innerHTML = `<a href="https://michaelfuchaka.github.io/my-personal-website/" target="_blank">View Web</a>`;
+  });
+
+  container.appendChild(button);
+});
 
     
